c37e7fbf81021859efbf0f2cd6c63baea57f1bd6
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-04-13 14:25:47 +1200

    RandomWords moved to new file. Hearts now added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,59 +14,5 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
         home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _randomWordPairs = <WordPair>[]; // Array of WordPairs from dependency
-
-  /**
-   * ListView Builder is a dynamic ListView
-   */
-  Widget _buildList() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, item) {
-        if (item.isOdd) return Divider();
-
-        final index = item ~/ 2; // Get number of wordPairs without Divider
-
-        if (index >= _randomWordPairs.length) {
-          _randomWordPairs.addAll(generateWordPairs().take(10));
-        }
-
-        return _buildRow(_randomWordPairs[index]);
-      },
-    );
-  }
-
-  /**
-   * Generates each ListTile needed in the ListView
-   */
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: TextStyle(fontSize: 18),
-      ),
-    );
-  }
-
-  /**
-   * Return app title 
-   * Uses the ListView method as the body
-   */
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("WordPair Generator"),
-      ),
-      body: _buildList(),
-    );
   }
 }
